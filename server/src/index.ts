dea035f1df9c00e2012ae1d7cd174a37ada6c270
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2021-02-06 20:48:40 +0000

    The actual fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,9 @@ import { RecipeIngredient } from "./entity/recipeIngredient";
 createConnection().then(async connection => {
     const queryRunner = connection.createQueryRunner();
     await queryRunner.connect();
-    await queryRunner.query("DELETE * FROM recipe_ingredient");
-    await queryRunner.query("DELETE * FROM recipe");
-    await queryRunner.query("DELETE * FROM ingredient");
+    await queryRunner.query("DELETE FROM recipe_ingredient");
+    await queryRunner.query("DELETE FROM recipe");
+    await queryRunner.query("DELETE FROM ingredient");
 
     console.log("Inserting a new recipe into the database")
     const recipe = new Recipe();
