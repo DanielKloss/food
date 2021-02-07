eb1a25b98037afc9d8056339925784a6d913ed86
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2021-02-07 22:57:44 +0000

    created insert ingredient

--- server/src/controller/ingredientController.ts
+++ server/src/controller/ingredientController.ts
@@ -1,15 +1,41 @@
 import { Repository } from "typeorm";
 import { Ingredient } from "../entity/Ingredient";
+import { Store } from "../entity/store";
+import { StoreIngredient } from "../entity/storeIngredient";
+import { Unit } from "../entity/unit";
 
 export class IngredientController {
 
-    repository: Repository<Ingredient>;
+    ingredientRepo: Repository<Ingredient>;
+    unitRepo: Repository<Unit>;
+    storeIngredientRepo: Repository<StoreIngredient>;
 
-    constructor(repository: Repository<Ingredient>){
-        this.repository = repository;
+    constructor(ingredientRepository: Repository<Ingredient>, unitRepository: Repository<Unit>, storeIngredientRepository: Repository<StoreIngredient>){
+        this.ingredientRepo = ingredientRepository;
+        this.unitRepo = unitRepository;
+        this.storeIngredientRepo = storeIngredientRepository;
     }
 
-    async InsertIngredients(ingredientsToAdd: Ingredient[]){
-        await this.repository.save(ingredientsToAdd);
+    async InsertIngredient(ingredientToAdd: Ingredient, storeAmounts: [Store, number][]){
+        let foundIngredient = await this.ingredientRepo.findOne({name: ingredientToAdd.name});
+        if (foundIngredient != undefined){
+            throw "Ingredient already exists";
+        }
+
+        let unit = await this.unitRepo.findOne({name: ingredientToAdd.unit.name})
+        if (unit){
+            ingredientToAdd.unit.id = unit.id;
+        }
+
+        let ingredient = await this.ingredientRepo.save(ingredientToAdd);
+
+        for (const storeAmount of storeAmounts) {
+            let storeIngredient = new StoreIngredient();
+            storeIngredient.ingredientId = ingredient.id;
+            storeIngredient.storeId = storeAmount[0].id;
+            storeIngredient.quantity = storeAmount[1];
+
+            await this.storeIngredientRepo.save(storeIngredient);
+        }
     }
 }
