93162385dffff742a9e9c9e3c730b116e6feb5ca
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2021-02-20 21:34:36 +0000

    get ingredients with store

--- server/src/controller/storeController.ts
+++ server/src/controller/storeController.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { createQueryBuilder } from "typeorm";
 import { Store } from "../entity/store";
 
 export class StoreController {
 
     static async GetAllStores(request: Request, response: Response){
-        let storeRepo = getRepository(Store);
-        response.send(await storeRepo.find());
+        response.send(
+            await createQueryBuilder<Store>("Store")
+            .innerJoinAndSelect("Store.storeIngredient", "storeIngredient")
+            .innerJoinAndSelect("storeIngredient.ingredient", "ingredient")
+            .innerJoinAndSelect("ingredient.unit", "unit")
+            .getMany()
+        );
     }
 
 }
