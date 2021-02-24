e47bc0c2d357978bc812524f2a3af1823213fa9c
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2021-02-24 11:20:31 +0000

    routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,6 +24,8 @@ createConnection().then(async () => {
     app.get("/api/stores", StoreController.GetAllStores);
     app.get("/api/storesAndQuantities", StoreController.GetAllStoresAndQuantities);
 
+    app.get("/api/ingredientsAndStores", IngredientController.getIngredientandStores);
+
     app.get("/api/ingredients", IngredientController.getAllIngredients)
     app.post("/api/ingredient", IngredientController.InsertIngredient);
     app.put("/api/stroreIngredients", IngredientController.UpdateStoreIngredientQuantity);
