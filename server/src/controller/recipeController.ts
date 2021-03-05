f575311b5d8d1df0a6bc406ca714947ae41db6c2
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2021-03-05 21:52:38 +0000

    actually logging now

--- server/src/controller/recipeController.ts
+++ server/src/controller/recipeController.ts
@@ -57,7 +57,7 @@ export class RecipeController {
 
     static async InsertRecipe(request: Request, response: Response){
         console.log("Adding recipe!");
-        util.inspect(request, false, null, true);
+        console.log(util.inspect(request, false, null, true));
         let recipeRepo = getRepository(Recipe);
         let tagRepo = getRepository(Tag);
         let instructionRepo = getRepository(Instruction);
