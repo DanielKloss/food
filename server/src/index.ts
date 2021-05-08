f38ed737bc16ea012e7bf043d9147cbc168752af
DanielKloss <arsenal_fan_dan@hotmail.com>
2021-05-08 11:45:12 +0100

    added slashes

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,22 +15,22 @@ createConnection().then(async () => {
     app.use(express.json());
     app.use(express.urlencoded());
 
-    app.get("foodApi/recipes", RecipeController.GetAllRecipes);
-    app.get("foodApi/recipeByName", RecipeController.GetRecipeIngredientsAndInstructionsByName);
-    app.get("foodApi/recipesByTag", RecipeController.GetRecipesByTag);
-    app.get("foodApi/recipesByIngredient", RecipeController.GetRecipesByIngredient);
-    app.post("foodApi/recipe", RecipeController.InsertRecipe);
+    app.get("/foodApi/recipes", RecipeController.GetAllRecipes);
+    app.get("/foodApi/recipeByName", RecipeController.GetRecipeIngredientsAndInstructionsByName);
+    app.get("/foodApi/recipesByTag", RecipeController.GetRecipesByTag);
+    app.get("/foodApi/recipesByIngredient", RecipeController.GetRecipesByIngredient);
+    app.post("/foodApi/recipe", RecipeController.InsertRecipe);
 
-    app.get("foodApi/stores", StoreController.GetAllStores);
-    app.get("foodApi/storesAndQuantities", StoreController.GetAllStoresAndQuantities);
-    app.get("foodApi/quantities", StoreController.GetAllQuantities);
+    app.get("/foodApi/stores", StoreController.GetAllStores);
+    app.get("/foodApi/storesAndQuantities", StoreController.GetAllStoresAndQuantities);
+    app.get("/foodApi/quantities", StoreController.GetAllQuantities);
 
-    app.get("foodApi/ingredients", IngredientController.getAllIngredients)
-    app.get("foodApi/units", IngredientController.getAllUnits);
-    app.get("foodApi/ingredientAndStores", IngredientController.getIngredientandStores);
-    app.put("foodApi/updateIngredient", IngredientController.UpdateStoreIngredientQuantity);
-    app.put("foodApi/updateStockIngredient", IngredientController.UpdateIngredientStock);
-    app.post("foodApi/insertIngredient", IngredientController.InsertIngredient);
+    app.get("/foodApi/ingredients", IngredientController.getAllIngredients)
+    app.get("/foodApi/units", IngredientController.getAllUnits);
+    app.get("/foodApi/ingredientAndStores", IngredientController.getIngredientandStores);
+    app.put("/foodApi/updateIngredient", IngredientController.UpdateStoreIngredientQuantity);
+    app.put("/foodApi/updateStockIngredient", IngredientController.UpdateIngredientStock);
+    app.post("/foodApi/insertIngredient", IngredientController.InsertIngredient);
 
     app.listen(port, () => {
         console.log("server started at localhost: " + port);
