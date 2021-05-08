3738310c2238284a1e2e5d5e549953a51845f2eb
DanielKloss <arsenal_fan_dan@hotmail.com>
2021-05-08 11:29:11 +0100

    changed back

--- app/src/app/services/store.service.ts
+++ app/src/app/services/store.service.ts
@@ -10,7 +10,7 @@ import { map } from 'rxjs/operators';
 })
 export class StoreService {
 
-  url = "http://192.168.1.229/foodApi/";
+  url = "http://86.15.198.129/foodApi/";
 
   constructor(private http: HttpClient) { }
 
